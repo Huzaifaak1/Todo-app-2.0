38dc8554df767b5fcc815ec25d26d486432e7102
Huzaifa Ali Khan <108281380+Huzaifaak1@users.noreply.github.com>
2023-07-11 20:02:14 +0500

    Update logic.js

--- logic.js
+++ logic.js
@@ -2,20 +2,30 @@ let card = document.getElementsByClassName('card');
 let cont = document.getElementById('container');
 
 let modal = document.getElementById('modal');
+let modal2 = document.getElementById('deleteBox');
+
 let closeBtn = document.getElementById('close');
 
+let cancelBtn = document.getElementById('cancel');
+
+let deleteBtn = document.getElementById('delete');
 
 closeBtn.addEventListener('click',()=>{
     modal.close();
 })
 
-
+cancelBtn.addEventListener('click',()=>{
+    modal2.close();
+})
 
 
 
 function something(){
     modal.showModal();
 }
+
+
+
 
 var colors = [
     "#FFEB3B",
@@ -31,7 +41,7 @@ var colors = [
   ];
 let deg = [2,-2,-3,3];
 
-
+let newCard;
 function addTodo(){
     let randomNumber = Math.floor(Math.random() * 10);
     let randomNumber2 = Math.floor(Math.random() * 4);
@@ -39,13 +49,14 @@ function addTodo(){
     let title = document.getElementById('heading');
     let desc = document.getElementById('desc');
 
-    let newCard = document.createElement('div');
+    newCard = document.createElement('div');
     let heading = document.createElement('h2');
     let para = document.createElement('p');
     var dot = document.createElement('span');
     dot.classList.add('addDot');
 
 
+    //Styling card dynamically
     newCard.style.height = "250px";
     newCard.style.transform = `rotate(${deg[randomNumber2]}deg)`;
     newCard.style.backfaceVisibility = "hidden";
@@ -57,10 +68,13 @@ function addTodo(){
     newCard.style.marginLeft = "1rem";
     newCard.style.marginTop = "2rem";
     newCard.style.boxShadow = "-3px 3px 4px 0px";
+    newCard.style.cursor = "pointer";
 
     newCard.style.backgroundColor = colors[randomNumber];
     heading.style.backgroundColor =  colors[randomNumber];
     para.style.backgroundColor =  colors[randomNumber];
+
+
     heading.style.fontFamily = "Grand Hotel, cursive";
     heading.style.color = "#000000";
     heading.style.zIndex = "2";
@@ -73,16 +87,41 @@ function addTodo(){
 
     heading.innerText = title.value;
     para.innerText = desc.value;
+
     newCard.appendChild(heading)
     newCard.appendChild(para);
     newCard.appendChild(dot);
+
+    //Delete Feature
+    deleteNote(newCard);
     
     newCard.style.zIndex = "1";
 
     cont.append(newCard);
 
+    
+
     modal.close();
 }
+
+const deleteNote = (card)=>{
+    card.addEventListener('click',()=>{
+        modal2.showModal();
+        deleteBtn.addEventListener('click',()=>{
+            card.style.display = "none";
+            modal2.close();
+        })
+    })
+}
+
+// const displayNone = (card)=>{
+//     deleteBtn.addEventListener('click',()=>{
+//         card.style.display = "none";
+//         modal2.close();
+//     })
+// }
+
+
 
 
 
